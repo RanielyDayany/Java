aecf12756a6ba98fef2d4f52a57567ad92985d4e
Raniely Dayany <ranielydayany58@gmail.com>
2025-05-28 17:47:52 -0300

    update
    Exercício 2 faculdade

---
+++ exerciciosDescomplica/ex02/Tipos/src/Tipos.java
@@ -0,0 +1,39 @@
+public class Tipos{
+    public static void main(String [] args){
+    //8 bits,intervalo de -128 a 127
+    byte oitoBits = 100;
+
+    //16 bits,intervalo de -32,768 a 32,767
+    short numeroMuitoPequeno = 32;
+
+    //32 bits,intervalo de -2^32 a 2^31-1
+    int inteiro = 32000;
+
+    //64 bits,intervalo de -2^63 a 2^63-1
+    long numeroMuitoLongo = 164000;
+
+    //32 bit,ponto flutuante de precisão simples
+    float decimalSimples = 31;
+
+    //64 bits,ponto flutuante de precisão dupla
+    double decimalPreciso = 34.5;
+
+    //16 bits caracter Unicode
+    char caractere = 'a';
+
+    //true ou false
+    boolean teste= true;
+  
+
+    System.out.println(oitoBits);
+    System.out.println(numeroMuitoPequeno);
+    System.out.println(inteiro);
+    System.out.println(numeroMuitoLongo);
+    System.out.println(decimalSimples);
+    System.out.println(decimalPreciso);
+    System.out.println(caractere);
+    System.out.println(teste);
+    }
+    
+
+}
