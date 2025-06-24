90f84580b42dda44ac31057fa88b661692a56c39
Raniely Dayany <ranielydayany58@gmail.com>
2025-06-24 19:08:43 -0300

    atualização exercícios
    Estrutura Switch,Instruções controle,Manipulação de matrizes e Manipulação de vetores

---
+++ exerciciosDescomplica/ex07/instrucoes/InstrucoesControle.java
@@ -0,0 +1,23 @@
+package exerciciosDescomplica.ex07.instrucoes;
+
+public class InstrucoesControle {
+     /*public static void main(String[] args){
+        System.out.println("BREAK");
+        for(int i = 0;i < 10;i++){
+            if(i == 5){
+                break;//Encerra o loop quando o i é igual a 5
+                //Usar o break pra simplesmente parar uma condição não é uma boa prática em JAVA
+            }
+            System.out.println("Iteração: "+ i);
+        }
+
+        System.out.println("\n CONTINUE");
+        for(int i = 0;i < 10;i++){
+            if(i % 2 == 0){
+                continue;//Pula a iteração para números pares
+            }
+            System.out.println("Iteração ímpar: "+i);
+        }
+    }*/
+
+}
---
+++ exerciciosDescomplica/ex07/instrucoes/ManipulacaoVetores.java
@@ -0,0 +1,20 @@
+package exerciciosDescomplica.ex07.instrucoes;
+
+public class ManipulacaoVetores {
+    /*public static void main(String[]args){
+        int[] numeros = {1,2,3,4,5};
+
+        int comprimento = numeros.length;//Obtem o comprimento do vetor
+        System.out.println("O comprimento do vetor é: " +comprimento);
+
+        System.out.println("\nMostrar cada elemento:implementação por extenso");
+        for(int i = 0; i < numeros.length;i++){
+            System.out.println(numeros[i]);//Imprime cada elemento de vetor
+        }
+        System.out.println("\nMostrar cada elemento:implementação simplificada");
+        for(int numero:numeros){//int item: items
+            System.out.println(numero);//Imprime cada elemento de vetor
+        }
+    }*/
+
+}
